e17c936a9cdc968d132a1b4c13fb934b01c7d08d
eapil16 <77381727+eapil16@users.noreply.github.com>
2022-11-16 13:37:28 +0300

    add distinctions page

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+    
+  // $(document).on('click','.video-btn',function(e){
+  //   e.preventDefault();
+  //   const video = $(this).closest(".videos-item").find(".video")[0];
+  //   const poster = $(this).closest(".videos-item").find(".videos-item__poster")[0];
+  //   $(poster).fadeOut();
+  //   if (video.paused) {
+  //     video.play();
+  //   } else {
+  //     video.pause();
+  //   }
+  // });
+
+});
